6ed6e0e91ffd7e157c01811b70c30f5a59d681de
HauntedHamburger <moreaerodynamic@gmail.com>
2024-09-07 13:35:44 -0500

    phonebook morgan update

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,17 +1,39 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
-app.use(express.json())
-
 const PORT = 3001
 
-const generateId = () => {
-    const maxId = persons.length > 0 
-        ? Math.floor(Math.random()) 
-        : 0
-    return maxId + 1
+// Basic usage
+app.use(morgan('combined'))
+
+// Custom token definition
+morgan.token('user-agent', (req, res) => {
+    return req.headers['user-agent']
+})
+
+// Express Json-Parser
+// >> Takes raw data from 'request object'
+// >> Parses into JS object
+// >> Assigns it to 'request object' as new property body
+app.use(express.json())
+
+// Morgan string format
+app.use(morgan(':method :url :status :response-time'))
+
+// Randomizer
+const generateRandom = () => {
+    return Math.floor(Math.random() * 100) + 1
 }
 
+// ID Creator
+const generateId = () => {
+    const maxId = persons.length > 0 
+        ? Math.max(...persons.map(p => Number(p.id)))
+        : 0
+    return String(maxId + generateRandom())
+}
+
+// Hardcoded Dataset
 let persons = [
     { 
       "id": "1",
@@ -35,6 +57,11 @@ let persons = [
     }
 ]
 
+
+// --------------------------------------------------------- //
+//                          ROUTES                           //
+// --------------------------------------------------------- //
+
 // Home
 app.get('/', (req, res) => {
     res.send('<p>Ayo</p>')
@@ -45,6 +72,7 @@ app.get('/api/persons', (req, res) => {
     res.send(persons)
 })
 
+// Get Single Person
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
     const person = persons.find(p => p.id === id)
@@ -56,22 +84,55 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+// Get Info
 app.get('/info', (req, res) => {
     res.send(`Phonebook has info for ${persons.length} people <br><br> ${Date()}`)
 })
 
+// Add Person
 app.post('/api/persons', (req, res) => {
     const body = req.body
     body.id = generateId()
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'missing a name'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'missing a number'
+        })
+    }
+
+    const existingName = persons.find(p => p.name === body.name)
+    if (existingName) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     persons = persons.concat(body)
     res.status(201).send(persons)
 })
 
+// Delete Person
 app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
     persons = persons.filter(p => p.id !== id)
     res.status(204).end()
 })
+
+// Undefined Route Catcher
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpount' })
+}
+
+app.use(unknownEndpoint)
+
+
+// --------------------------------------------------------- //
+//                       LISTENER                            //
+// --------------------------------------------------------- //
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
